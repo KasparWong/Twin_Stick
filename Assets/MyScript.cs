69adfed17491901e79f9d693efd3cc397bd1c031
Kaspar Wong <github@kasparwong.com>
2015-11-21 02:26:20 +0800

    Added script

---
+++ Assets/MyScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        print("Hi");
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
